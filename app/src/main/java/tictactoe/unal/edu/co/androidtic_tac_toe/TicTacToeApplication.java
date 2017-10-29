07de197d5615041bd05906fdb82c9845c58c6f5f
hninor <hninor@unal.edu.co>
2017-10-29 16:51:12 -0500

    start firebase

---
+++ app/src/main/java/tictactoe/unal/edu/co/androidtic_tac_toe/TicTacToeApplication.java
@@ -0,0 +1,18 @@
+package tictactoe.unal.edu.co.androidtic_tac_toe;
+
+import android.app.Application;
+
+
+
+/**
+ * Created by USER on 29/10/2017.
+ */
+
+public class TicTacToeApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
